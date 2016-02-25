d2aa765ad2ec7f280a515b981576aae757537827
Howard Ding <hding2@illinois.edu>
2016-02-25 13:34:39 -0600

    Set some capistrano stuff, though most will have to wait until some more setup is done on the remote side.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'medusa-downloader'
+set :repo_url, 'https://github.com/medusa-project/medusa-downloader.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -35,6 +35,12 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :keep_releases, 5
 
 namespace :deploy do
+
+  namespace :deploy do
+    task :restart => 'monit:restart'
+  end
+
+  after 'deploy:publishing', 'deploy:restart'
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do

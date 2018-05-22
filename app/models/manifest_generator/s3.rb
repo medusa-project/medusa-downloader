7520a5344bb834cdda99acb23eb7312df519f7b7
Howard Ding <hding2@illinois.edu>
2018-05-22 15:52:58 -0500

    Downloads for s3 "directories" Some refactoring towards extracing storage root for more general use.

--- app/models/manifest_generator/s3.rb
+++ app/models/manifest_generator/s3.rb
@@ -2,89 +2,76 @@ class ManifestGenerator::S3 < ManifestGenerator::Base
 
   delegate :bucket, :region, :prefix, to: :storage_root
 
-  def s3_client
-    @s3_client ||= storage_root.s3_client
-  end
-
-  def presigner
-    @presigner ||= Aws::S3::Presigner.new(client: s3_client)
-  end
 
   def add_file(target)
     path = target['zip_path'] || ''
     name = target['name'] || File.basename(target['path'])
     zip_file_path = File.join(path, name)
     key = key_for(target['path'])
-    info = s3_client.head_object(bucket: bucket, key: key)
-    size = info.content_length
-    file_url = presigner.presigned_url(:get_object, bucket: bucket, key: key, expires_in: 7.days.to_i)
+    size = storage_root.size(key)
+    file_url = storage_root.presigned_get_url(key)
     self.file_list << [file_url, zip_file_path, size, false]
   rescue Aws::S3::Errors::NotFound
     raise InvalidFileError(request.root, target['path'])
   end
 
   def add_directory(target)
-    #TODO the main problem here is to get all of the keys under the target. In the worst case we can set this up as a
-    # call out to rclone, but hopefully the AWS SDK  provides a way to do it directly. It looks like list_objects_v2
-    # with a set prefix ang pagination will work for
-    # recursive listing. It may be harder to do it for non recursive grabbing.
-    # For recursive we do client.list_objects_v2(bucket: bucket, prefix: prefix). This will have keys in contents.keys. Then as long
-    # as we have a next_continuation_token, keep redoing the request.
-    # For non-recursive we might just have to get all the keys and filter them - chop off the prefix and get everything that
-    # doesn't have a slash.
-    # Then we need to add them to the file list in the right way. So there are some details here, but everything should
-    # be doable. It may be easier than for the filesystem because we have the complete key already.
-    # We may also get keys ending in '/', which we want to reject
-    key_prefix = key_for(target['path'])
-    key_prefix = key_prefix + '/' unless key_prefix.end_with?('/')
-    keys = Array.new
-    continuation_token = nil
-    #This gets only those in the specified 'directory' unless recursion is specified
-    delimiter = (target['recursive'] == true) ? nil : '/'
-    loop do
-      results = s3_client.list_objects_v2(bucket: bucket, prefix: key_prefix, continuation_token: continuation_token, delimiter: delimiter)
-      keys += results.contents.collect(&:key).reject {|key| key.end_with?('/')}
-      continuation_token = results.next_continuation_token
-      break if continuation_token.nil?
-    end
-    #add to the file list for each key, similarly to add_file but preserving the right path info and such
-    #once again, we'll need to get the size and presigned url. May admit some refactoring after.
-  end
-
-  raise "Not yet implemented"
-end
-
-def write_file_list_and_compute_size
-  self.total_size = 0
-  File.open(manifest_path, 'wb') do |f|
-    self.file_list.each.with_index do |spec, i|
-      path, zip_path, size, literal = spec
-      self.total_size += size
-      final_path = "#{request.zip_name}/#{zip_path}".gsub(/\/+/, '/')
-      if literal
-        symlink_path = File.join(data_path, i.to_s)
-        FileUtils.symlink(path, symlink_path)
-        f.write "- #{size} /internal#{relative_path_to(symlink_path)} #{final_path}\r\n"
-      else
-        f.write "- #{size} #{normalized_path(path)} #{final_path}\r\n"
+    key = key_for(target['path'])
+    keys = if target['recursive'] == true
+             storage_root.subtree_keys(key)
+           else
+             storage_root.file_keys(key)
+           end
+    zip_path = target['zip_path'] || target['path']
+    keys.each do |key|
+      begin
+        zip_file_path = File.join(zip_path, relative_path(key, target['path']))
+        size = storage_root.size(key)
+        file_url = storage_root.presigned_get_url(key)
+        self.file_list << [file_url, zip_file_path, size, false]
+      rescue Aws::S3::Errors::NotFound
+        raise InvalidFileError(request.root, key)
       end
     end
   end
-end
 
-def key_for(path)
-  if prefix.blank?
-    path
-  else
-    File.join(prefix, path)
+  def write_file_list_and_compute_size
+    self.total_size = 0
+    File.open(manifest_path, 'wb') do |f|
+      self.file_list.each.with_index do |spec, i|
+        path, zip_path, size, literal = spec
+        self.total_size += size
+        final_path = "#{request.zip_name}/#{zip_path}".gsub(/\/+/, '/')
+        if literal
+          symlink_path = File.join(data_path, i.to_s)
+          FileUtils.symlink(path, symlink_path)
+          f.write "- #{size} /internal#{relative_path_to(symlink_path)} #{final_path}\r\n"
+        else
+          f.write "- #{size} #{normalized_path(path)} #{final_path}\r\n"
+        end
+      end
+    end
   end
-end
+
+  def key_for(path)
+    if prefix.blank?
+      path
+    else
+      File.join(prefix, path)
+    end
+  end
 
 #convert a url like https://dls-medusa-test.s3.us-east-2.amazonaws.com/nfs_lock_test.sh?params to
 # /<bucket>/nfs_lock_test.sh?params
-def normalized_path(path)
-  truncated_path = path.gsub(/^(.*?)amazonaws.com\//, '')
-  "/#{bucket}/#{truncated_path}"
-end
+  def normalized_path(path)
+    truncated_path = path.gsub(/^(.*?)amazonaws.com\//, '')
+    "/#{bucket}/#{truncated_path}"
+  end
+
+  def relative_path(full_key, directory)
+    directory = directory + '/' unless directory.end_with?('/')
+    prefix_to_remove = key_for(directory)
+    full_key.sub(/^#{prefix_to_remove}/, '')
+  end
 
 end
--- app/models/storage_root/s3.rb
+++ app/models/storage_root/s3.rb
@@ -15,11 +15,69 @@ class StorageRoot::S3 < StorageRoot
   end
 
   def s3_client
-    Aws::S3::Client.new(region: region, credentials: s3_credentials)
+    @s3_client ||= Aws::S3::Client.new(region: region, credentials: s3_credentials)
   end
 
   def s3_credentials
     Aws::Credentials.new(aws_access_key_id, aws_secret_access_key)
+  end
+
+  def presigner
+    @presigner ||= Aws::S3::Presigner.new(client: s3_client)
+  end
+
+  def info(key)
+    s3_client.head_object(bucket: bucket, key: key)
+  end
+
+  def size(key)
+    info(key).content_length
+  end
+
+  def presigned_get_url(key, args = {})
+    presigner.presigned_url(:get_object, {bucket: bucket, key: key, expires_in: 7.days_to_i}.merge(args))
+  end
+
+  def file_keys(directory_key)
+    internal_subtree_keys(directory_key, delimiter: '/')
+  end
+
+  def subtree_keys(directory_key)
+    internal_subtree_keys(directory_key)
+  end
+
+  def subdirectory_keys(directory_key)
+    Array.new.tap do |keys|
+      continuation_token = nil
+      loop do
+        results = s3_client.list_objects_v2(bucket: bucket, prefix: ensure_directory_key(directory_key), continuation_token: continuation_token, delimiter: delimiter)
+        keys += results.common_prefixes.collect(&:key)
+        continuation_token = results.next_continuation_token
+        break if continuation_token.nil?
+      end
+    end
+  end
+
+  private
+
+  def internal_subtree_keys(directory_key, delimiter: nil)
+    Array.new.tap do |keys|
+      continuation_token = nil
+      loop do
+        results = s3_client.list_objects_v2(bucket: bucket, prefix: ensure_directory_key(directory_key), continuation_token: continuation_token, delimiter: delimiter)
+        keys += results.contents.collect(&:key).reject {|key| directory_key?(key)}
+        continuation_token = results.next_continuation_token
+        break if continuation_token.nil?
+      end
+    end
+  end
+
+  def directory_key?(key)
+    key.end_with?('/')
+  end
+
+  def ensure_directory_key(key)
+    directory_key?(key) ? key : key + '/'
   end
 
 end

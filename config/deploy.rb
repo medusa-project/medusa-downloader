ce0a0030c071b1443546b1d2f820afdffb7b866f
Howard Ding <hding2@illinois.edu>
2018-12-11 13:24:14 -0600

    update cap lock version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.10.1'
+lock '3.11.0'
 
 set :application, 'medusa-downloader'
 set :repo_url, 'https://github.com/medusa-project/medusa-downloader.git'

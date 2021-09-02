320942ccab075b21263cfadfd960e504b51cbbd7
Gen Schmitt <gschmitt@illinois.edu>
2021-09-02 14:29:19 -0400

    refine application status

--- app/models/application_status.rb
+++ app/models/application_status.rb
@@ -1,7 +1,10 @@
+require 'json'
+
 class ApplicationStatus < Object
     def self.query_application_status
         rclone_status = `monit status rclone-mount | grep status | head -n 1 |  awk '{ print $2; }'`
         rclone_path = `cat .monitrc | grep 'rclone-mount path' | awk '{ print $5; }' | sed -e 's/^"//' -e 's/"$//'`
         rclone_mounted = `ls #{rclone_path} | wc -l`
         json_response = {"Rclone Monit Status" => rclone_status, "Rclone Mount Status" => rclone_mounted}.to_json
-    end    
+    end   
+end     

3bf7dfb1229daddb6b9d79be121d4396f9ace0fc
Gen Schmitt <gschmitt@illinois.edu>
2026-05-15 10:54:55 -0600

    Updated Gemfile platforms and Capfile lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.19.2'
+lock '3.20.0'
 
 set :application, 'medusa-downloader'
 set :repo_url, 'https://github.com/medusa-project/medusa-downloader.git'

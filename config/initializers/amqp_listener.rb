4985ea608a1824283dd6117da66bcc19a34f1709
Gen Schmitt <gschmitt@illinois.edu>
2026-06-05 13:19:59 -0600

    Updated app status page to show amqp and delayed job status

--- config/initializers/amqp_listener.rb
+++ config/initializers/amqp_listener.rb
@@ -2,14 +2,17 @@ if defined?(PhusionPassenger)
   PhusionPassenger.on_event(:starting_worker_process) do |forked|
     if forked
       begin
+        AmqpListenerState.mark_starting!
         connection = Bunny.new(Settings.amqp.to_h)
         connection.start
         Kernel.at_exit do
+          AmqpListenerState.mark_stopped!
           connection.close rescue nil
         end
         Rails.logger.error 'Starting AMQP listener'
         channel = connection.create_channel
         queue = channel.queue(Settings.incoming_queue, durable: true)
+        AmqpListenerState.mark_running!(connection: connection, queue_name: Settings.incoming_queue)
         queue.subscribe do |delivery_info, properties, payload|
           begin
             AmqpRequestBridge.create_request(payload)
@@ -19,6 +22,7 @@ if defined?(PhusionPassenger)
           end
         end
       rescue Exception => e
+        AmqpListenerState.mark_error!(e)
         Rails.logger.error "Unknown error starting AMQP listener: #{e}"
       end
     end
